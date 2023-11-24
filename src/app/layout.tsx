622105fe43e5d65c1fbf5bc672200aecb2b07274
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-11-24 16:58:50 +0100

    Initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Simonyi Konferencia 2024',
   description: 'Magyarország legnagyobb, egyetemi hallgatók által szervezett éves technológiai konferenciája.',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='hu'>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }

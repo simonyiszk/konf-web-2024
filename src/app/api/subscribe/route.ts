7d0e767a2c505396e3dd8fadbb58884ef9e8e7d7
Trisztán Piller <piller.trisztan@simonyi.bme.hu>
2023-12-01 21:15:13 +0100

    finally working

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,45 +1,34 @@
-import axios, { AxiosError } from 'axios';
-import { JWT } from 'google-auth-library';
+import { google } from 'googleapis';
 import { NextRequest, NextResponse } from 'next/server';
+
 export interface Subscription {
   email: string;
 }
+
 export async function POST(request: NextRequest) {
   const { email }: Subscription = await request.json();
 
-  const credential = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT_JSON ?? '', 'base64').toString());
-
-  const client = new JWT({
-    email: credential.client_email,
-    key: credential.private_key,
-    scopes: ['https://www.googleapis.com/auth/admin.directory.group'],
+  const credentials = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT_JSON ?? '', 'base64').toString());
+  const auth = new google.auth.JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    subject: process.env.SERVICE_ACCOUNT_JSON,
+    scopes: ['https://www.googleapis.com/auth/admin.directory.group.member'],
   });
 
-  const { token } = await client.getAccessToken();
-
-  const memberData = {
-    email,
-    role: 'MEMBER',
-  };
-
   try {
-    const response = await axios.post(
-      `https://admin.googleapis.com/admin/directory/v1/groups/${encodeURIComponent(
-        'konf-news@simonyi.bme.hu'
-      )}/members`,
-      memberData,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const response = await google.admin({ version: 'directory_v1', auth }).members.insert({
+      groupKey: 'konf-news@simonyi.bme.hu',
+      requestBody: {
+        email,
+        role: 'MEMBER',
+      },
+    });
     console.log(response);
     return new NextResponse('Created', { status: 201 });
   } catch (e) {
-    if (e instanceof AxiosError) {
-      console.log(e);
-    }
+    console.log(e);
+    console.log('error: ', (e as any).response?.data?.error);
     return new NextResponse(':(', { status: 500 });
   }
 }

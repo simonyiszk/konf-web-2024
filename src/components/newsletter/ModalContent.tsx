348910511b95a1c480fdd83c89bb389ed6915da5
Sámuel Fekete <feketesamu@gmail.com>
2023-12-02 23:16:42 +0100

    typos, minimal styling

--- src/components/newsletter/ModalContent.tsx
+++ src/components/newsletter/ModalContent.tsx
@@ -32,13 +32,29 @@ export function ModalContent() {
 
   return (
     <div>
-      <input type='checkbox' id='accept' checked={accepted} onChange={(e) => setAccepted(e.target.checked)} />
+      <input
+        className='mr-2'
+        type='checkbox'
+        id='accept'
+        checked={accepted}
+        onChange={(e) => setAccepted(e.target.checked)}
+      />
       <label htmlFor='accept'>
-        Beleegyezem, hogy a konferencáig körülbelül havonta két emailt fogok kapni az alábbi email címre.
+        Beleegyezem, hogy a konferenciáig havonta maximum két emailt fogok kapni az alábbi email címre.
       </label>
-      <div className='flex justify-between items-center gap-2'>
-        <input className='text-black' value={email} onChange={(e) => setEmail(e.target.value)} type='email' />
-        <button disabled={!email || !accepted} onClick={onSubscribe} className='bg-white text-black rounded-md p-3'>
+      <div className='flex justify-center items-center gap-2'>
+        <input
+          placeholder='valami@email.com'
+          className='text-black p-1 rounded-md'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+        />
+        <button
+          disabled={!email || !accepted}
+          onClick={onSubscribe}
+          className='bg-white text-black rounded-md p-3 disabled:bg-gray-400 disabled:cursor-not-allowed'
+        >
           Feliratkozás
         </button>
       </div>

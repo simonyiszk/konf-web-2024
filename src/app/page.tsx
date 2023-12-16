c6373bd841f5f055365f1e6f061db0b36806c661
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-12-16 14:05:43 +0100

    feat: layout improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,21 @@ import konfLogo from '../../public/img/konf.svg';
 export default function Landing() {
   return (
     <>
-      <div className='shadow-gloria rounded-full overflow-hidden'>
-        <video className='h-full w-full' autoPlay playsInline loop controls={false}>
-          <source src='/video/nebula.mp4' type='video/mp4' />
-        </video>
-      </div>
-      <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center flex-col py-5 gap-5 p-5'>
-        <Image src={konfLogo} alt='Simonyi Konferencia' className='w-full' />
-        <p className='font-bold text-xl sm:text-2xl text-center'>{metadata.description}</p>
-        <p className='font-semibold text-2xl sm:text-6xl'>24. 03. 19.</p>
-        <div className='flex md:hidden flex-col items-center gap-10 mt-10'>
-          <SocialButtons />
-          <NewsletterModals />
+      <div className='p-10 relative'>
+        <div className='max-w-md md:max-w-xl relative shadow-gloria rounded-full overflow-hidden mx-auto'>
+          <video className='h-full w-full' autoPlay playsInline loop controls={false}>
+            <source src='/video/nebula.mp4' type='video/mp4' />
+          </video>
         </div>
+        <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center flex-col gap-5 p-5'>
+          <Image src={konfLogo} alt='Simonyi Konferencia' className='w-full' />
+          <p className='font-bold text-xl sm:text-2xl text-center'>{metadata.description}</p>
+          <p className='font-semibold text-4xl sm:text-6xl'>24. 03. 19.</p>
+        </div>
+      </div>
+      <div className='flex md:hidden flex-col items-center gap-10 mt-10'>
+        <SocialButtons />
+        <NewsletterModals />
       </div>
     </>
   );

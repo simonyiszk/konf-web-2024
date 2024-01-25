789ba109ff4197dc19e0cfb8845c6bd1c937165b
Sámuel Fekete <feketesamu@gmail.com>
2024-01-25 18:29:13 +0100

    use same function to get data

---
+++ src/models/get-index-data.ts
@@ -0,0 +1,9 @@
+import { IndexPageData } from './models';
+
+export async function getIndexData(): Promise<IndexPageData> {
+  const res = await fetch(`${process.env.BACKEND_URL}/conference/index`);
+  if (!res.ok) {
+    throw new Error(res.status.toString());
+  }
+  return res.json();
+}

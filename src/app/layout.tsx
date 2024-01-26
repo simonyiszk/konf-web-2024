705479817e8cb5276671ef89ac43059c26a28300
Sámuel Fekete <feketesamu@gmail.com>
2024-01-26 10:48:22 +0100

    waiting for recharge font files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Raleway } from 'next/font/google';
 
+//import localFont from 'next/font/local';
 import { Footer } from '@/components/footer/footer';
 import { Navbar } from '@/components/navbar/navbar';
 
-const raleway = Raleway({ subsets: ['latin'] });
+export const raleway = Raleway({ subsets: ['latin'], variable: '--font-raleway' });
+//export const recharge = localFont({ src: 'TODO', variable: '--font-recharge' });
 
 export const metadata: Metadata = {
   title: 'Simonyi Konferencia - 2024. 03. 19.',
@@ -18,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='hu'>
-      <body className={raleway.className}>
+      <body>
         <main className='flex flex-col min-h-screen'>
           <Navbar />
           <div className='flex-grow relative flex flex-col justify-center items-center'>{children}</div>

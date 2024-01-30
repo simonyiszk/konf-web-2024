7611446964d14a65f4a2465499586e90619dde6d
Sámuel Fekete <feketesamu@gmail.com>
2024-01-30 11:55:12 +0100

    revalidate after 30 mins

--- src/models/get-index-data.ts
+++ src/models/get-index-data.ts
@@ -1,7 +1,7 @@
 import { IndexPageData } from './models';
 
 export async function getIndexData(): Promise<IndexPageData> {
-  const res = await fetch(`${process.env.BACKEND_URL}/conference/index`, { next: { revalidate: 300 } });
+  const res = await fetch(`${process.env.BACKEND_URL}/conference/index`, { next: { revalidate: 30 * 60 } });
   if (!res.ok) {
     throw new Error(res.status.toString());
   }

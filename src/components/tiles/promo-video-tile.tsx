71cfd839e0968b321149f7c506289d3d7cdaa739
Sámuel Fekete <feketesamu@gmail.com>
2024-02-03 22:32:10 +0100

    added promo video

--- src/components/tiles/promo-video-tile.tsx
+++ src/components/tiles/promo-video-tile.tsx
@@ -6,20 +6,19 @@ type Props = {
   data: PromoVideoData;
 };
 
-export function PromoVideoTile({ data: { description, sectionTitle, youtubeUrl } }: Props) {
+export function PromoVideoTile({ data: { description, sectionTitle } }: Props) {
   return (
     <Tile className='sm:col-span-6'>
       <Tile.Body>
         <h2 className='text-5xl sm:text-6xl font-bold'>{sectionTitle}</h2>
         <div className='mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4'>
           <p className='text-lg sm:text-xl font-medium text-justify w-full'>{description}</p>
-          <iframe
+          <video
             className='w-full aspect-[16/9]'
-            src={youtubeUrl}
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-            allowFullScreen
-            title='Simonyi Konferencia promó videó'
-          />
+            controls
+            src='/video/promo_video.mp4'
+            poster='/img/thumbnail.png'
+          ></video>
         </div>
       </Tile.Body>
     </Tile>

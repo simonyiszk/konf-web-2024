e9d7cfb840e2f288107095ef20a7a7ebe2ff8780
Sámuel Fekete <feketesamu@gmail.com>
2024-02-05 20:19:57 +0100

    apply tile styling to markdown, maybe fix #26

--- src/app/giveaway/page.tsx
+++ src/app/giveaway/page.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 import { GiveawayTile } from '@/components/tiles/giveaway-tile';
+import { Tile } from '@/components/tiles/tile';
 import { getIndexData } from '@/models/get-index-data';
 
 export default async function Giveaway() {
@@ -15,9 +16,11 @@ export default async function Giveaway() {
       <h1 className='col-span-6'>Nyereményjáték</h1>
       <GiveawayTile data={data.giveaway} showLink={false} />
 
-      <div className='tile col-span-6 markdown'>
-        <ReactMarkdown children={data.giveaway.rules} remarkPlugins={[remarkGfm]} />
-      </div>
+      <Tile className='col-span-6'>
+        <Tile.Body className='markdown'>
+          <ReactMarkdown children={data.giveaway.rules} remarkPlugins={[remarkGfm]} />
+        </Tile.Body>
+      </Tile>
     </div>
   );
 }

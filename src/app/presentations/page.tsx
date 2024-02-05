70fffc58ee3ae3daf33d11bd15fafbc32eacf3ec
Dkrisztan <krisztidancs@gmail.com>
2024-02-05 16:32:13 +0100

    changes based on figma design

--- src/app/presentations/page.tsx
+++ src/app/presentations/page.tsx
@@ -13,16 +13,20 @@ export default async function Presentations() {
         {presentations.map((presentation) => (
           <div className='flex flex-wrap justify-center'>
             <div className='w-full flex justify-center mb-16 md:mb-0'>
-              <div className='relative w-full max-w-[384px] bg-[#101010] shadow-md rounded-lg overflow-hidden'>
+              <div className='relative w-[410px] h-[598px] bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-800 to-[#010101] shadow-md border-[1px] rounded-3xl overflow-hidden'>
                 <Link href={`/presentations/${presentation.slug}`}>
-                  <img src={presentation.presenter.pictureUrl} className='object-cover' alt='Presentation Image' />
-                  <div className='p-4'>
-                    <h2 className='text-center text-3xl font-bold text-blue-300 pb-4'>{presentation.title}</h2>
-                    <h3 className='text-center text-3xl font-bold'>{presentation.presenter.name}</h3>
-                    <p className='text-center text-2xl pb-5'>
-                      {presentation.presenter.rank + ' @ ' + presentation.presenter.company?.name}
+                  <img
+                    src={presentation.presenter.pictureUrl}
+                    className='object-cover w-full h-[410px] rounded-3xl'
+                    alt='Presentation Image'
+                  />
+                  <div className='z-20 absolute text-center top-2/3 left-1/2 -translate-x-[50%] -translate-y-[30%] shadow-md rounded-xl overflow-hidden bg-[#FFE500]'>
+                    <p className='pt-1 min-w-[260px] min-h-[44px] text-[24px] text-black font-bold'>
+                      {presentation.presenter.name}
                     </p>
-                    <p className='absolute bottom-0 left-0 m-2'>📌 {presentation.room}</p>
+                  </div>
+                  <div className='p-6'>
+                    <h2 className='text-center text-[24px] font-bold text-white pb-4'>{presentation.title}</h2>
                   </div>
                 </Link>
               </div>

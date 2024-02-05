7a8002d7b6f9c6c81ecd092b2f53c21774e155f0
Dkrisztan <krisztidancs@gmail.com>
2024-02-05 16:35:25 +0100

    typo (xd)

--- src/app/presentations/[slug]/page.tsx
+++ src/app/presentations/[slug]/page.tsx
@@ -56,7 +56,6 @@ export default async function PresentationBySlug({ params }: { params: { slug: s
                     />
                   </div>
                 )}
-                )
               </div>
             </div>
           </div>

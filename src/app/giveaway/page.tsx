7806ef5b6130698441d82504436da1c3df1899f4
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-05 21:25:19 +0100

    adjust giveaway conditions text style

--- src/app/giveaway/page.tsx
+++ src/app/giveaway/page.tsx
@@ -16,7 +16,20 @@ export default async function Giveaway() {
       <GiveawayTile data={data.giveaway} showLink={false} />
 
       <div className='tile col-span-6 markdown'>
-        <ReactMarkdown children={data.giveaway.rules} remarkPlugins={[remarkGfm]} />
+        <ReactMarkdown
+          children={data.giveaway.rules}
+          remarkPlugins={[remarkGfm]}
+          components={{
+            h1: ({ children }) => <h1 className='text-5xl font-bold mb-8'>{children}</h1>,
+            h2: ({ children }) => <h2 className='text-4xl font-bold my-4'>{children}</h2>,
+            p: ({ children }) => <p className='my-1'>{children}</p>,
+            a: ({ children, href }) => (
+              <a className='text-konf-yellow underline' href={href as string} target='_blank' rel='noopener noreferrer'>
+                {children}
+              </a>
+            ),
+          }}
+        />
       </div>
     </div>
   );

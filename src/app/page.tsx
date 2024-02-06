6616fe62af05ddf53900dafb60f810e3caa2b0a7
Sámuel Fekete <feketesamu@gmail.com>
2024-02-06 21:19:38 +0100

    does this help?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Landing() {
     <>
       <div className='p-10 relative'>
         <div className='max-w-md md:max-w-xl relative shadow-gloria rounded-full overflow-hidden mx-auto'>
-          <video className='h-full w-full' autoPlay playsInline loop muted>
+          <video className='h-full w-full' autoPlay playsInline loop muted poster='/img/nebula-thumbnail.png'>
             <source src='/video/nebula.mp4' type='video/mp4' />
           </video>
         </div>

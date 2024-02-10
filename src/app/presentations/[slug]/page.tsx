cca230469ea200dc5cc89eeb5a15cc4c4a2481af
Dkrisztan <krisztidancs@gmail.com>
2024-02-10 13:43:01 +0100

    should be alright

--- src/app/presentations/[slug]/page.tsx
+++ src/app/presentations/[slug]/page.tsx
@@ -1,19 +1,17 @@
-import Link from 'next/link';
 import { notFound } from 'next/navigation';
-import { FaArrowLeft } from 'react-icons/fa';
 
+import Presentation from '@/components/presentation/Presentation';
 import { getIndexData } from '@/models/get-index-data';
 import slugify from '@/utils/slugify';
 
 export async function generateStaticParams() {
   const data = await getIndexData();
 
-  const slugs =
+  return (
     data?.presentations?.map((p) => ({
       slug: slugify(p.title),
-    })) ?? [];
-
-  return slugs;
+    })) ?? []
+  );
 }
 
 export async function getPresentationBySlug(slug: string) {
@@ -21,81 +19,11 @@ export async function getPresentationBySlug(slug: string) {
   return data?.presentations.find((p) => slugify(p.title) === slug);
 }
 
-export default async function PresentationBySlug({
-  params,
-}: {
-  params: { slug: string; isFrontPage?: boolean; imageUrls?: string[] };
-}) {
+export default async function PresentationBySlug({ params }: { params: { slug: string } }) {
   const presentation = await getPresentationBySlug(params.slug);
   if (!presentation) {
     notFound();
   }
 
-  const { title, description, presenter } = presentation;
-
-  let imageClass = 'object-cover rounded-3xl';
-
-  if (params.isFrontPage) {
-    imageClass += ' w-96 h-96';
-  } else {
-    imageClass += ' w-[308px] h-[308px]';
-  }
-
-  return (
-    <>
-      <div className='max-w-6xl w-full px-6 xl:px-0'>
-        {!params.isFrontPage && (
-          <h3 className='mb-5 w-fit hover:text-brand'>
-            <Link href={`/presentations`}>
-              <div className='flex items-center'>
-                <FaArrowLeft />
-                <p className='ml-1'>Vissza az előadásokhoz </p>
-              </div>
-            </Link>
-          </h3>
-        )}
-        {!params.isFrontPage && <h1 className='mb-16'>{title}</h1>}
-        <div className='flex flex-col md:flex-row gap-8'>
-          {!params.isFrontPage && <p className='text-stone-200 text-[20px] whitespace-pre-line'>{description}</p>}
-          {params.isFrontPage && (
-            <div>
-              <p className='mb-12 text-[40px] font-bold leading-10'>{title}</p>
-              <p className='text-stone-200 text-[20px] whitespace-pre-line'>
-                {description.slice(0, Math.floor(description.length / 2))}
-              </p>
-              <div className='flex'>
-                {params.imageUrls?.map((image) => {
-                  return (
-                    <img
-                      src={image}
-                      alt='presentation images'
-                      className='p-2 max-w-full max-h-[75px] object-fill mt-5'
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          )}
-          <div
-            className={`flex flex-col items-center flex-shrink-0 text-center ${
-              params.isFrontPage ? '' : 'order-first'
-            } md:order-last`}
-          >
-            <img src={presenter.pictureUrl} className={imageClass} alt='Presentation Image' />
-            <p className='block mt-4 text-[32px] leading-tight font-bold text-white-900'>{presenter.name}</p>
-            <p className='block mt-0.5 text-[20px]  text-[#FFE500]'>{presenter.rank}</p>
-            {presenter.company && (
-              <div className='mt-1.5 bg-white rounded-xl max-w-[308px] max-h-[75px]'>
-                <img
-                  src={presenter.company.logoUrl}
-                  alt='Company logo'
-                  className='p-2 max-w-full max-h-[75px] object-fit m-auto'
-                />
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <Presentation presentation={presentation} />;
 }

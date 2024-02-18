511d7faf4dda3a7d6af1bfbce1b6e2619ac04460
Sámuel Fekete <feketesamu@gmail.com>
2024-02-18 15:57:24 +0100

    padding fix

--- src/components/tiles/organizer-tile.tsx
+++ src/components/tiles/organizer-tile.tsx
@@ -7,10 +7,10 @@ type Props = Organiser;
 export function OrganiserTile({ name, emailAddress, pictureUrl, rank }: Props) {
   return (
     <Tile>
-      <Tile.Body lessPadding='2'>
+      <Tile.Body lessPadding='3'>
         <div className='flex flex-col h-full'>
           <img src={pictureUrl} className='w-full aspect-square object-cover object-center' />
-          <div className='rounded-b-[30px] pt-4 px-3 pb-9 flex flex-col h-full  gap-2 bg-[#101010]'>
+          <div className='rounded-b-[30px] pt-4 px-3 pb-9 flex flex-col h-full gap-2 bg-[#101010]'>
             <h2 className='text-center text-3xl font-bold'>{name}</h2>
             <p className='text-center text-2xl'>{rank}</p>
             <p className='text-center text-base sm:text-sm md:text-base lg:text-sm xl:text-base text-[#FFE500]'>

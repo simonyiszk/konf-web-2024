b0bc663855bf1efd5ca523f267e81cc2b8780cf9
Sámuel Fekete <feketesamu@gmail.com>
2024-02-20 23:44:55 +0100

    fic margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Landing() {
   }
   return (
     <>
-      <div className='mt-[120px] md:mt-0 p-10 relative'>
+      <div className='md:mt-0 p-10 relative'>
         <div className='max-w-md md:max-w-xl relative shadow-gloria rounded-full overflow-hidden mx-auto'>
           <video className='h-full w-full' autoPlay playsInline loop muted poster='/img/nebula-thumbnail.webp'>
             <source src='/video/nebula.mp4' type='video/mp4' />
--- src/components/navbar/navbar-mobile.tsx
+++ src/components/navbar/navbar-mobile.tsx
@@ -15,21 +15,23 @@ export function NavbarMobile() {
     if ((e.target as HTMLElement).tagName === 'A') setIsOpen(false);
   };
   return (
-    <nav className='w-full p-5 md:hidden fixed top-0 z-20' onClick={onLinkClick}>
-      <div className='w-full p-5 box-border rounded-md bg-white bg-opacity-10 backdrop-blur-md'>
-        <div className='w-full flex items-center justify-between gap-5'>
-          <Link href='/' onClick={() => setIsOpen(false)}>
-            <Image src='/img/icon-base.svg' alt='Simonyi Konferencia' width={40} height={40} />
-          </Link>
-          <button onClick={toggle}>
-            <TbMenu2 size={40} />
-          </button>
-        </div>
-        {isOpen && (
-          <div className={clsx('flex flex-col w-full gap-3 overflow-hidden box-border mt-5')}>
-            <NavbarItems />
+    <nav className='mb-[120px] md:hidden'>
+      <div className='w-full p-5 fixed top-0 z-20' onClick={onLinkClick}>
+        <div className='w-full p-5 box-border rounded-md bg-white bg-opacity-10 backdrop-blur-md'>
+          <div className='w-full flex items-center justify-between gap-5'>
+            <Link href='/' onClick={() => setIsOpen(false)}>
+              <Image src='/img/icon-base.svg' alt='Simonyi Konferencia' width={40} height={40} />
+            </Link>
+            <button onClick={toggle}>
+              <TbMenu2 size={40} />
+            </button>
           </div>
-        )}
+          {isOpen && (
+            <div className={clsx('flex flex-col w-full gap-3 overflow-hidden box-border mt-5')}>
+              <NavbarItems />
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );

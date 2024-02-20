eb8009d7e4f4e40b4bd00ba44071eb45d899c69b
Sámuel Fekete <feketesamu@gmail.com>
2024-02-20 23:38:00 +0100

    more stylig fixes

--- src/app/giveaway/page.tsx
+++ src/app/giveaway/page.tsx
@@ -13,7 +13,6 @@ export default async function Giveaway() {
   }
   return (
     <div className='grid sm:grid-col-6 max-w-6xl w-full my-10 px-4 sm:px-6 xl:px-0 gap-y-20'>
-      <h1 className='sm:col-span-6'>Nyereményjáték</h1>
       <GiveawayTile data={data.giveaway} showLink={false} />
       <Tile className='sm:col-span-6'>
         <Tile.Body className='markdown'>
@@ -21,7 +20,9 @@ export default async function Giveaway() {
             children={data.giveaway.rules}
             remarkPlugins={[remarkGfm]}
             components={{
-              h1: ({ children }) => <h1 className='text-2xl sm:text-4xl md:text-5xl font-bold mb-8'>{children}</h1>,
+              h1: ({ children }) => (
+                <h1 className='giveaway-title text-2xl sm:text-4xl md:text-5xl font-bold mb-8'>{children}</h1>
+              ),
               h2: ({ children }) => <h2 className='text-xl sm:text-3xl md:text-4xl font-bold my-4'>{children}</h2>,
               p: ({ children }) => <p className='my-1'>{children}</p>,
               a: ({ children, href }) => (

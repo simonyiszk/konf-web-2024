570b87a5f8b1821522eee171a11b70827a33ea72
FearsomeRover <bujgergo@gmail.com>
2024-03-02 11:28:30 +0100

    typo, flex col fixes

--- src/components/tiles/mobil-app-tile.tsx
+++ src/components/tiles/mobil-app-tile.tsx
@@ -14,10 +14,10 @@ export function MobilAppTile({ data }: Props) {
         <div className='flex flex-col gap-4 lg:gap-0 lg:flex-row w-full justify-between mt-4 items-center'>
           <img src='img/app-screens.png' className='object-cover rounded-3xl h-[300px] mr-3' alt='App screens' />
           <p className='text-stone-200 text-[20px] whitespace-pre-line w-full h-full items-center'>
-            Töltsd le a mobil appot, hogy élőben köveshesd a konferencia eseményeit és kérdezhess az előadóktól!
+            Töltsd le a mobil appot, hogy élőben követhesd a konferencia eseményeit és kérdezhess az előadóktól!
           </p>
         </div>
-        <div className='w-full flex lg:flex-row md:flex-col justify-center lg:gap-10 md:gap-5 items-center mt-4'>
+        <div className='w-full flex lg:flex-row flex-col justify-center lg:gap-10 gap-4 items-center mt-4'>
           <a href={data.androidUrl} className='h-fit'>
             <img className='h-[57px] min-w-[190]' src='img/google-play-badge-hu.png' alt='Play Store' />
           </a>

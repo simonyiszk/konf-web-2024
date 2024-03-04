0f942ff31a96a6379b2f6a63555bf6cd556b8728
Sámuel Fekete <sfeke@cae.com>
2024-03-04 12:41:28 +0100

    hotfix again

--- src/utils/dateHelper.ts
+++ src/utils/dateHelper.ts
@@ -1,4 +1,7 @@
 export function dateToHourAndMinuteString(date: Date): string {
   // https://imgflip.com/i/8hrgho
-  return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+  return date.toLocaleTimeString('hu', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 }

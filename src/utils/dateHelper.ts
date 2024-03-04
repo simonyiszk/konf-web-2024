018cb258d5046b7d7f5a717e123acad1175c35c3
Sámuel Fekete <sfeke@cae.com>
2024-03-04 12:49:53 +0100

    hotfix again^2

--- src/utils/dateHelper.ts
+++ src/utils/dateHelper.ts
@@ -3,5 +3,6 @@ export function dateToHourAndMinuteString(date: Date): string {
   return date.toLocaleTimeString('hu', {
     hour: '2-digit',
     minute: '2-digit',
+    timeZone: 'Europe/Budapest',
   });
 }

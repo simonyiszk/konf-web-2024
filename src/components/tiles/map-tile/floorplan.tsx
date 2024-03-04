1fdc2adbe648e0d93b7f4472356934d78980c59d
FearsomeRover <bujgergo@gmail.com>
2024-03-04 11:09:29 +0100

    increased map size on phone

--- src/components/tiles/map-tile/floorplan.tsx
+++ src/components/tiles/map-tile/floorplan.tsx
@@ -70,7 +70,7 @@ export function Floorplan() {
             );
           })}
         </ol>
-        <div className='pointer-events-none relative select-none bg-white/10 rounded-md flex-1 w-full mdx:w-fit p-8 max-w-[500px] flex flex-row justify-center items-center'>
+        <div className='pointer-events-none relative select-none bg-white/20 0 p-8 rounded-md md:flex-1 flex-0 w-full mdx:w-fit mdx:p-2 mdx:max-h-[1000px] max-h-full flex flex-row justify-center items-center'>
           <Map active={active} />
         </div>
       </div>

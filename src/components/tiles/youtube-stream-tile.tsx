374c019fb294bd3555f991bc32dbecc8a045f0fe
MBarni458 <mbarni458@gmail.com>
2024-03-10 18:03:52 +0100

    change title design

--- src/components/tiles/youtube-stream-tile.tsx
+++ src/components/tiles/youtube-stream-tile.tsx
@@ -9,7 +9,7 @@ export function StreamTile({ title, url }: Props) {
   return (
     <Tile className='sm:col-span-6 lg:col-span-3'>
       <Tile.Body>
-        <h2 className='sm:text-2xl md:text-4xl'>{title}</h2>
+        <h2 className='sm:text-2xl md:text-4xl text-center mb-5 capitalize'>{title}</h2>
         <div className='sm:w-full md:w-6/7 mx-auto mt-2'>
           <YoutubeVideo title={title} url={url} />
         </div>

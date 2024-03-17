133eded333faa133800efd4c4d699316b5218101
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-17 13:49:46 +0100

    feat: mobile promo tile v2

--- src/components/tiles/mobile/mobile-promotion-tile.tsx
+++ src/components/tiles/mobile/mobile-promotion-tile.tsx
@@ -7,30 +7,22 @@ export function MobilePromotionTile() {
     <>
       <Tile>
         <Tile.Body>
-          <p>Maradj naprakész a KonferenciApp segítségével - a Simonyi Konferencia legújabb társa!</p>
-          <p>
+          <p className='mb-2'>Maradj naprakész a KonferenciApp segítségével - a Simonyi Konferencia legújabb társa!</p>
+          <p className='mb-2'>
             Légy mindig képben a programtervvel, kapj valós idejű híreket, navigálj könnyedén interaktív térképek
             segítségével, és fedezd fel az egyéb izgalmas funkciókat, hogy még jobb legyen a konferencia élményed.
           </p>
-          <p>Töltsd le most, és hozd ki a legtöbbet a Simonyi Konferenciából!</p>
-          <div className='flex items-center'>
-            <Image className='-ml-10 -mb-10' src='/img/question.png' alt='Kérdezés' height={200} width={200} />
-            <p className='m-0'>
-              Kérdezz az előadótól az alkalmazásban! Egy előadás oldalán található buborék ikonra kattintva máris a
-              kérdezői felületre jutsz.
-            </p>
-          </div>
+          <p className='mb-2'>Töltsd le most, és hozd ki a legtöbbet a Simonyi Konferenciából!</p>
+          <p className='mb-2'>
+            <strong>Kérdezz az előadótól az alkalmazásban!</strong> Egy előadás oldalán található buborék ikonra
+            kattintva máris a kérdezői felületre jutsz.
+          </p>
+          <Image className='mx-auto -mb-10' src='/img/question.png' alt='Kérdezés' height={600} width={282} />
         </Tile.Body>
       </Tile>
       <Tile>
-        <Tile.Body>
-          <Image
-            src='/img/konferenciapp.png'
-            alt='KonferenciApp'
-            width={500}
-            height={509}
-            className='h-72 w-auto mx-auto'
-          />
+        <Tile.Body className='flex items-center justify-center'>
+          <Image src='/img/konferenciapp.png' alt='KonferenciApp' width={500} height={509} className='h-80 w-auto' />
         </Tile.Body>
       </Tile>
     </>

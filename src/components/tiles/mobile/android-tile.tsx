3df1f3a6594f3c09c63dbe8e3ae7b26bc9b403cc
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-17 22:51:00 +0100

    feat: disable android download link *sad violin starts playing*

--- src/components/tiles/mobile/android-tile.tsx
+++ src/components/tiles/mobile/android-tile.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { Tile } from '@/components/tiles/tile';
 
@@ -7,19 +6,20 @@ interface AndroidTileProps {
   androidUrl: string;
 }
 
-export function AndroidTile({ androidUrl }: AndroidTileProps) {
+export function AndroidTile({}: AndroidTileProps) {
   return (
     <Tile>
       <Tile.Body className='space-y-8 text-center'>
         <h2>Android</h2>
-        <Link className='mx-auto w-fit block' href={androidUrl}>
-          <Image src='/img/androidapk.svg' alt='Letölthető Android APK' width={302} height={80} />
-        </Link>
-        <p>
-          Az APK fájlt letöltve és megnyitva telepítheted az alkalmazást az Androidos eszközödre. A telepítés
-          előtt/közben az "Ismeretlen forrásból származó alkalmazások engedélyezése" opciót be kell kapcsolni a
-          beállításokban.
-        </p>
+        <Image
+          className='opacity-50 mx-auto'
+          src='/img/google-play.png'
+          alt='Letölthető Android APK'
+          width={250}
+          height={40}
+        />
+        <p>Sajnáljuk, de az Android alkalmazás még nem érhető el a Google Play Áruházban.</p>
+        <p>További információkért látogass el a Kir-Dev standjához!</p>
       </Tile.Body>
     </Tile>
   );

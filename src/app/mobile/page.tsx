2e52e27595045e6c42d3fb3b783f0b06a6ff3785
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-17 12:31:52 +0100

    feat: mobile app question prompt

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -8,7 +8,7 @@ export default async function MobilePage() {
   return (
     <div className='max-w-6xl w-full px-4 sm:px-6 xl:px-0'>
       <h1 className='mb-10'>KonferenciApp</h1>
-      <div className='grid grid-cols-1 sm:grid-cols-2 auto-rows-auto gap-10'>
+      <div className='grid grid-cols-1 md:grid-cols-2 auto-rows-auto gap-10'>
         <MobilePromotionTile />
         <IosTile iosUrl={data?.mobilApp.iosUrl ?? ''} />
         <AndroidTile androidUrl={data?.mobilApp.androidUrl ?? ''} />
--- src/components/tiles/mobile/mobile-promotion-tile.tsx
+++ src/components/tiles/mobile/mobile-promotion-tile.tsx
@@ -6,13 +6,20 @@ export function MobilePromotionTile() {
   return (
     <>
       <Tile>
-        <Tile.Body className='space-y-4'>
+        <Tile.Body>
           <p>Maradj naprakész a KonferenciApp segítségével - a Simonyi Konferencia legújabb társa!</p>
           <p>
             Légy mindig képben a programtervvel, kapj valós idejű híreket, navigálj könnyedén interaktív térképek
             segítségével, és fedezd fel az egyéb izgalmas funkciókat, hogy még jobb legyen a konferencia élményed.
           </p>
           <p>Töltsd le most, és hozd ki a legtöbbet a Simonyi Konferenciából!</p>
+          <div className='flex items-center'>
+            <Image className='-ml-10 -mb-10' src='/img/question.png' alt='Kérdezés' height={200} width={200} />
+            <p className='m-0'>
+              Kérdezz az előadótól az alkalmazásban! Egy előadás oldalán található buborék ikonra kattintva máris a
+              kérdezői felületre jutsz.
+            </p>
+          </div>
         </Tile.Body>
       </Tile>
       <Tile>
